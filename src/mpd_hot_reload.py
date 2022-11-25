5356091131cbc64c84458d364ac4354958ba9c70
Harsh Sharma <68972644+codelif@users.noreply.github.com>
2022-11-25 23:55:48 +0530

    Add License info on top of every source file

--- src/mpd_hot_reload.py
+++ src/mpd_hot_reload.py
@@ -1,5 +1,5 @@
 """
-src/cache.py - Caching playlist names for verbose purpose.
+src/mpd_hot_reload.py - Updates MPD Library with minimal interruption.
 
 Copyright (C) 2022  Harsh Sharma  <goharsh007@gmail.com>
 
@@ -17,25 +17,3 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
-
-import os
-import json
-from utils import ensure_folder, ensure_sync_file
-
-
-CACHE_FILE = os.path.expanduser("~/.cache/ypsync")
-ensure_folder(CACHE_FILE)
-
-
-def cache_playlist(name: str, play_id: str):
-    path = os.path.join(CACHE_FILE, "playlists.json")
-    ensure_sync_file(path)
-    
-    with open(path, "r+") as f:
-        obj = json.load(f)
-        print(obj)
-        obj[play_id] = name
-        f.truncate(0)
-        f.seek(0)
-        json.dump(obj, f)
-        
